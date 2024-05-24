ff6fe041b826dbd94e0b6184fc60c4a085a58533
Benny Mendoza <bennybc9387@gmail.com>
2024-05-24 14:04:20 -0600

    renaming and switching view engines

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require('express')
 const app = new express()
 const path = require('path')
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+  res.render('index')
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+  res.render('about')
 })
 app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+  res.render('contact')
 })
 app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+  res.render('post')
 })
 
 app.listen(3000, () => {
   console.log('app listening on 3000')
 })
-
-// this isn't working.. for sure it is now
