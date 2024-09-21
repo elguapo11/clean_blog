01e5938823518e01ab0d5a1aa05a027fa512ac41
Benny Mendoza <bennybc9387@gmail.com>
2024-09-21 10:06:25 -0600

    added models and established connection, created posts

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.render('index')
 })
+
 mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
 
 app.get('/about', (req, res) => {
