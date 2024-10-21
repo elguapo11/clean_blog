6d965a4ff053c364734683b8223fe20c3dbb1c1c
Benny Mendoza <bennybc9387@gmail.com>
2024-10-20 20:06:34 -0600

    refactored, will do validation next, mid chapter 9

--- index.js
+++ index.js
@@ -5,32 +5,35 @@ const mongoose = require('mongoose')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const BlogPost = require('./public/models/BlogPost')
+const newPostController = require('./public/controllers/newPost')
+
+const homeController = require('./public/controllers/home')
+const storePostController = require('./public/controllers/storePost')
+const getPostController = require('./public/controllers/getPost')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
-
 const fileUpload = require('express-fileupload')
 app.use(fileUpload())
-
-
-
-
 mongoose.connect('mongodb://localhost/my_database')
 app.use(express.static('public'))
-
+app.get('posts/new', newPostController)
 app.get('/', async (req, res) => {
   const blogposts = await BlogPost.find({})
   res.render('index', { blogposts })
 })
 
 
+app.get('/', homeController)
+
+app.get('/post/:id',getPostController)
+
+app.post('/posts/store', storePostController)
+
 app.get('/about', (req, res) => {
   res.render('about')
 })
 
-app.get('/posts/new', (req, res) => {
-  res.render('create')
-})
 app.get('/contact', (req, res) => {
   res.render('contact')
 })
@@ -42,24 +45,6 @@ const validationMiddleWare = (req, res, next) => {
   next()
 }
 app.use('/posts/store', validationMiddleWare)
-
-app.get('/post/:id', async (req, res) => {
-  const blogpost = await BlogPost.findById(req.params.id)
-  res.render('post',{ blogpost })
-})
-app.post('/posts/store', async (req, res) => {
-  let image = req.files.image;
-  image.mv(path.resolve(__dirname, 'public/img', image.name), async (error) => {
-    await BlogPost.create({
-      ...req.body,
-      image: '/img/' + image.name
-    })
-    console.log(image)
-    res.redirect('/')
-  })
-
-})
-
 app.delete('/posts/:id', async (req, res) => {
   const blogpost = await BlogPost.findById(req.params.id)
   console.log(blogpost + 'has been deleted')
