437b4b114d5bb5b7a54581fe7bbd16423b058090
Benny Mendoza <bennybc9387@gmail.com>
2024-10-26 12:47:18 -0600

    added unique properties to mongo, added error handling

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,8 +4,12 @@ const path = require('path')
 module.exports = async (req, res) => {
   try {
       const user = await User.create(req.body);
+      if(error) {
+        return res.redirect('/auth/register')
+      }
       res.redirect('/');
   } catch (error) {
+      console.log(error)
       res.redirect('/auth/register');
   }
 };
--- models/User.js
+++ models/User.js
@@ -3,15 +3,22 @@ const Schema = mongoose.Schema
 const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
-  username: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 })
 
-UserSchema.pre('save',function(next){
+UserSchema.pre('save', function (next) {
   const user = this
-  bcrypt.hash(user.password, 10,(error, hash)=>{
-      user.password = hash
-      next()
+  bcrypt.hash(user.password, 10, (error, hash) => {
+    user.password = hash
+    next()
   })
 })
 
