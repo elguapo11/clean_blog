e7a910b829b598fde983d25a0eb8f30b2828cf30
Benny Mendoza <bennybc9387@gmail.com>
2024-10-30 16:09:37 -0600

    username/password validation errors are present

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -1,15 +1,19 @@
-const User = require('../models/User.js')
-const path = require('path')
+const User = require('../models/User.js');
+const path = require('path');
 
 module.exports = async (req, res) => {
   try {
-      const user = await User.create(req.body);
-      if(error) {
-        return res.redirect('/auth/register')
-      }
-      res.redirect('/');
+    const user = await User.create(req.body);
+    res.redirect('/');
   } catch (error) {
-      console.log(error)
-      res.redirect('/auth/register');
+    if (error.name === 'ValidationError') {
+      const validationErrors = Object.keys(error.errors).map(
+        key => error.errors[key].message
+      );
+      // Pass errors to the view
+      return res.render('register', { errors: validationErrors });
+    }
+    console.log(error);
+    res.redirect('/auth/register');
   }
 };
