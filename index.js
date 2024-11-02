c98ca8e1878f4c6695a73c78161f0a78b55097a9
Benny Mendoza <bennybc9387@gmail.com>
2024-11-01 20:20:44 -0600

    index re-ordered, working

--- index.js
+++ index.js
@@ -1,78 +1,68 @@
 const express = require('express')
-const app = new express()
 const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser: true})
+
+const app = new express()
 const ejs = require('ejs')
+const { resourceUsage } = require('process')
+
+const fileUpload = require('express-fileupload')
+const validateMiddleWare = require('./middleware/validationMiddleware')
 const expressSession = require('express-session')
-const flash = require('connect-flash')
-const bodyParser = require('body-parser')
-const BlogPost = require('./models/BlogPost')
-const storeUserController = require('./controllers/storeUser')
+const authMiddleware = require('./middleware/authMiddleware')
+const redirectIfAuthenticatedMiddleware = require('./middleware/redirectIfAuthenticatedMiddleware')
+
+app.set('view engine','ejs')
+
+global.loggedIn = null;
+
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(fileUpload())
+app.use('/posts/store',validateMiddleWare)
+app.use(expressSession({
+    secret: 'keyboard cat'
+}))
+app.use("*",(req,res,next)=>{
+    loggedIn = req.session.userId
+    next()
+})
+
+app.listen(3000,()=>{
+    console.log('Lstening on port 3000')
+})
+
 const newPostController = require('./controllers/newPost')
-const validationMiddleWare = require('./middleware/validationMiddleware')
-const newUserController = require('./controllers/newUser')
 const homeController = require('./controllers/home')
 const storePostController = require('./controllers/storePost')
 const getPostController = require('./controllers/getPost')
+const newUserController = require('./controllers/newUser')
+const storeUserController = require('./controllers/storeUser')
 const loginController = require('./controllers/login')
 const loginUserController = require('./controllers/loginUser')
-const authMiddleware = require('./middleware/authMiddleware')
-const redirectIfAuthenticatedMiddleware = require('./middleware/redirectIfAuthenticatedMiddleware')
 const logoutController = require('./controllers/logout')
 
-
-
-app.set('view engine', 'ejs')
-app.use(flash());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-const fileUpload = require('express-fileupload')
-const User = require('./models/User')
-app.use(fileUpload())
-mongoose.connect('mongodb://localhost/my_database')
-app.use(express.static('public'))
-app.use(expressSession({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
-app.use('/posts/store', validationMiddleWare)
-app.get('posts/new', newPostController)
-app.get('/auth/register', redirectIfAuthenticatedMiddleware, newUserController)
 app.get('/', homeController)
-app.get('/posts/new', newPostController)
+
 app.get('/post/:id',getPostController)
-app.get('/auth/login', redirectIfAuthenticatedMiddleware, loginController)
-app.get('/posts/new', authMiddleware, newPostController)
-app.get('/contact', (req, res) => {
-  res.render('contact')
-})
-app.get('/about', (req, res) => {
-  res.render('about')
-})
-app.get('auth/logout', logoutController)
-app.post('/posts/store', newPostController, storePostController)
-app.post('/users/register', redirectIfAuthenticatedMiddleware, storeUserController)
-app.post('/users/login', redirectIfAuthenticatedMiddleware, loginUserController)
 
+app.get('/posts/new',authMiddleware, newPostController)
 
-app.delete('/posts/:id', async (req, res) => {
-  const blogpost = await BlogPost.findById(req.params.id)
-  console.log(blogpost + 'has been deleted')
-  res.sendStatus(200)
-})
-app.use((req, res) => res.render('notFound'))
-app.listen(3000, () => {
-  console.log('app listening on 3000')
-})
+app.post('/posts/store',authMiddleware, storePostController)
 
-global.loggedIn = null
+app.get('/auth/register',redirectIfAuthenticatedMiddleware,newUserController)
 
-app.use((req, res, next) => {
-  res.locals.messages = req.flash();
-  next();
-});
+app.post('/users/register',redirectIfAuthenticatedMiddleware,storeUserController)
 
-app.use('*', (req, res, next) => {
-  loggedIn = req.session.userId
-  next()
+app.get('/auth/login',redirectIfAuthenticatedMiddleware, loginController)
+
+app.post('/users/login',redirectIfAuthenticatedMiddleware,loginUserController)
+
+app.get('/auth/logout', logoutController)
+
+app.use((req,res)=>{
+    res.render('notfound')
 })
