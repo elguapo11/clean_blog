9c09242b39c835a1adbf2ce341a7f55371291e92
Benny Mendoza <bennybc9387@gmail.com>
2025-01-17 13:03:38 -0700

    does this work?

--- controllers/google.js
+++
@@ -1,3 +0,0 @@
-module.exports = (req, res) => {
-    res.render('/Users/bennymendoza/Documents/coding_projects/clean_blog/startbootstrap-clean-blog-gh-pages/views/layouts/google649d1d1bc4bfbce8.html')
-}
