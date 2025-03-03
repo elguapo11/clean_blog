6b639079e6c49e6a8af951c9da87841200081373
Benny Mendoza <bennybc9387@gmail.com>
2025-03-03 11:34:49 -0700

    added photo preview

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -27,3 +27,41 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let images = document.querySelectorAll("div img");
+    let modalImage = document.getElementById("modalImage");
+    let currentIndex = 0;
+
+    function openModal(index) {
+      currentIndex = index;
+      updateModalImage();
+      new bootstrap.Modal(document.getElementById("imageModal")).show();
+    }
+
+    function updateModalImage() {
+      modalImage.src = images[currentIndex].src;
+    }
+
+    function prevImage() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      updateModalImage();
+    }
+
+    function nextImage() {
+      currentIndex = (currentIndex + 1) % images.length;
+      updateModalImage();
+    }
+
+    // Attach click event to each image
+    images.forEach((img, index) => {
+      img.addEventListener("click", function () {
+        openModal(index);
+      });
+    });
+
+    // Expose functions to the global scope so they can be used in the modal
+    window.prevImage = prevImage;
+    window.nextImage = nextImage;
+  });
