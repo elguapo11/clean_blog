7a3e70ae2f5b0c13d87f591ee428cc3fcb75b728
Benny Mendoza <bennybc9387@gmail.com>
2025-07-03 18:06:19 -0600

    removed dependencies

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 require("dotenv").config();
 const compression = require('compression')
 const express = require('express')
-const mongoose = require('mongoose')
-const flash = require('connect-flash');
 
-const { PORT, SECRET, CONNECTION_STRING, } = process.env;
-
-mongoose.connect(CONNECTION_STRING)
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('Connection error:', error));
-
-
+const { PORT, SECRET, CONNECTION_STRING } = process.env
 
 // this connection string is going to change depending on the env
-
 
 const app = new express()
 const ejs = require('ejs')
@@ -23,7 +14,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.json())
 
-app.use(flash())
+
 app.use('*', (req, res, next) => {
   next()
 })
